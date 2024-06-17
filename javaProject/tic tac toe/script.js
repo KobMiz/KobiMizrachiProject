60890a73d4def221b713e8a22f541f145396a1dc
KobMiz <kobimizrachi@icloud.com>
2024-06-17 13:14:21 +0300

    X or O fixing

--- javaProject/tic tac toe/script.js
+++ javaProject/tic tac toe/script.js
@@ -1,6 +1,7 @@
-const divs = document.querySelectorAll("#board>div");
+const divs = document.querySelectorAll("#board > div");
 let isX = true;
 let isGameOver = false;
+let startingPlayer = true; 
 
 const scores = {
     x: localStorage.x ? Number(localStorage.x) : 0,
@@ -25,27 +26,20 @@ divs.forEach(div => {
         const clickSound = new Audio('click.mp3');
         clickSound.play();
 
-
-        if (isX) {
-            elem.innerText = "X";
-        } else {
-            elem.innerText = "O";
-        }
-
+        elem.innerText = isX ? "X" : "O";
         isX = !isX;
         showTurn();
         checkWinner();
     });
-})
-
+});
 
 function showTurn() {
     document.querySelector('.currentTurn').classList.remove('currentTurn');
 
     if (isX) {
-        document.querySelector("#players>div:first-child").classList.add('currentTurn');
+        document.querySelector("#players > div:first-child").classList.add('currentTurn');
     } else {
-        document.querySelector("#players>div:last-child").classList.add('currentTurn');
+        document.querySelector("#players > div:last-child").classList.add('currentTurn');
     }
 }
 
@@ -61,48 +55,52 @@ function checkWinner() {
         [2, 4, 6],
     ];
 
+    let winnerFound = false;
+
     for (const op of options) {
         if (op.every(x => divs[x].innerText === 'X')) {
-            scores.x++;
-            winner(op, 'X');
-            document.querySelector("#x_score").innerText = scores.x;
+            handleWin(op, 'X');
+            winnerFound = true;
             break;
         } else if (op.every(x => divs[x].innerText === 'O')) {
-            scores.o++;
-            winner(op, 'O');
-            document.querySelector("#o_score").innerText = scores.o;
-            break;
-        } else if ([...divs].every(x => x.innerText)) {
-            setTimeout(() => alert("אין מנצח"), 50);
-            isGameOver = true;
+            handleWin(op, 'O');
+            winnerFound = true;
             break;
         }
     }
+
+    if (!winnerFound && [...divs].every(x => x.innerText)) {
+        setTimeout(() => alert("אין מנצח"), 50);
+        isGameOver = true;
+    }
 }
 
-function winner(op, win) {
+function handleWin(op, win) {
     op.forEach(x => divs[x].classList.add('win'));
 
     isGameOver = true;
 
-    localStorage.x = scores.x;
-    localStorage.o = scores.o;
+    if (win === 'X') {
+        scores.x++;
+        localStorage.x = scores.x;
+        document.querySelector("#x_score").innerText = scores.x;
+    } else {
+        scores.o++;
+        localStorage.o = scores.o;
+        document.querySelector("#o_score").innerText = scores.o;
+    }
 
     const winnerSpan = document.getElementById('winnerSpan');
     winnerSpan.innerText = `המנצח הוא - ${win}`;
     winnerSpan.style.display = 'block';
-    winnerSpan.style.margin = '20px auto';
 
     const winSound = new Audio('applause.mp3');
     winSound.play();
 
     setTimeout(() => {
         winnerSpan.style.display = 'none';
-        isX = !isX;
     }, 3000);
 }
-
-
 
 
 function resetScores() {
@@ -115,12 +113,23 @@ function resetScores() {
         localStorage.o = 0;
         document.querySelector("#x_score").innerText = scores.x;
         document.querySelector("#o_score").innerText = scores.o;
-
-        newGame();
+        resetGame();
     }
 }
 
-function newGame() {
+function resetScores() {
+    const isConfirmed = confirm("האם אתה בטוח שברצונך לאפס את הניקוד?");
+
+    if (isConfirmed) {
+        scores.x = 0;
+        scores.o = 0;
+        document.querySelector("#x_score").innerText = scores.x;
+        document.querySelector("#o_score").innerText = scores.o;
+        resetGame();
+    }
+}
+
+function resetGame() {
     divs.forEach(div => {
         div.innerText = '';
         div.classList.remove('win');
@@ -132,15 +141,43 @@ function newGame() {
     }, 1000);
 
     isGameOver = false;
+    isX = startingPlayer; 
     showTurn();
 }
 
-function isBoardFull() {
-    for (const div of divs) {
-        if (!div.innerText) {
-            return false;
-        }
+function newGame() {
+    divs.forEach(div => {
+        div.innerText = '';
+        div.classList.remove('win');
+    });
+
+    startingPlayer = !startingPlayer;
+
+    document.body.classList.add('reset-background');
+    setTimeout(() => {
+        document.body.classList.remove('reset-background');
+    }, 1000);
+
+    isGameOver = false;
+    isX = startingPlayer;
+    showTurn();
+}
+
+window.addEventListener('load', () => {
+    if (localStorage.x) {
+        scores.x = Number(localStorage.x);
+        document.querySelector("#x_score").innerText = scores.x;
     }
 
-    return true;
-}
+    if (localStorage.o) {
+        scores.o = Number(localStorage.o);
+        document.querySelector("#o_score").innerText = scores.o;
+    }
+});
+
+isX = startingPlayer;
+showTurn();
+
+
+
+
